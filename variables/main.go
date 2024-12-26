c26d0cbe19dcfbbf9aa47e521fc5c0d3d3a42763
HelloItsSandeep <191733681+HelloItsSandeep@users.noreply.github.com>
2024-12-26 21:43:11 +0530

    go variables

---
+++ variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName string = "Sandeep"
+	myInt := 10
+	myFloat := 10.0
+
+	fmt.Printf("Hello my name is %s my int is %d my float is %f\n", myName, myInt, myFloat)
+
+	var myFriendsName string
+	var myBool bool
+	var myOtherInt int
+
+	fmt.Printf("My other friend name %s my bool %t and myother init %d\n", myFriendsName, myBool, myOtherInt)
+}
